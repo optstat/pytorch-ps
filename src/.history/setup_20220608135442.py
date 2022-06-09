f1adbda3da5fd9b6f9d41dc978878b44c49f507a
Panos Lambrianides <panos.sanfrancisco@gmail.com>
2022-06-08 21:02:27 -0700

    Removed history files

--- src/.history/setup_20220608135442.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='ps_cpp',
-      ext_modules=[cpp_extension.CppExtension('ps_cpp', ['ps.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
--- src/.history/setup_20220608164354.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='ps_cpp',
-      ext_modules=[cpp_extension.CppExtension('ps_cpp', ['ps.cpp', 'polylib.cpp'])],
-      extra_compile_args=["-z", "defs"],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
